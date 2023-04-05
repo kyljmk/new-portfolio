cb5970a2fffc6959f55da36c5c6655b6db6fc220
kyljmk <k.mansonkullin@googlemail.com>
2023-04-05 11:21:09 +0200

    Add Socials Icons To Menu And Menu Slide Functionality

--- new-portfolio/src/components/Navbar.tsx
+++ new-portfolio/src/components/Navbar.tsx
@@ -1,11 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
 import Image from "next/image";
 import Link from "next/link";
-import { AiOutlineClose, AiOutlineMenu } from "react-icons/ai";
+import {
+  AiOutlineClose,
+  AiOutlineMenu,
+  AiOutlineLinkedin,
+} from "react-icons/ai";
+import { VscGithub } from "react-icons/vsc";
+import { FiMail } from "react-icons/fi";
+import { BsPersonLinesFill } from "react-icons/bs";
 
 function Navbar() {
+  const [nav, setNav] = useState<boolean>(false);
+
+  const handleNav = () => {
+    setNav((prev) => !prev);
+  };
+
   return (
-    <div className="fixed w-full h-20 shadow-xl z-[100]">
+    <div className="fixed w-full h-20 shadow-xl z-[100] ease-in duration-300">
       <div className="flex justify-between items-center w-full h-full px-2 2xl:px-16">
         <div className="flex">
           <div className="bg-[#3C4F76] rounded-full text-[#EFF6EE] font-bold w-10 h-10 flex items-center justify-center text-2xl">
@@ -40,31 +53,84 @@ function Navbar() {
               </li>
             </Link>
           </ul>
-          <div className="md:hidden">
+          <div className="cursor-pointer md:hidden" onClick={handleNav}>
             <AiOutlineMenu size={25} />
           </div>
         </div>
       </div>
-      <div className="fixed left-0 top-0 w-full bg-black/70">
-        <div className="left-0 top-0 w-[75%] sm:w-[60%] md:w-[45%] h-screen bg-[#AB9F9D] p-10 ease-in duration-500">
+      <div
+        className={
+          nav ? "md:hidden fixed left-0 top-0 w-full h-screen bg-black/70" : ""
+        }
+      >
+        <div
+          className={
+            nav
+              ? "fixed left-0 top-0 w-[75%] sm:w-[60%] md:w-[45%] h-screen bg-[#EFF6EE] p-10 ease-in duration-500"
+              : "fixed left-[-100%] top-0 p-10 ease-in duration-500"
+          }
+        >
           <div className="flex w-full items-center justify-between">
             <div className="flex">
-              <div className="bg-[#3C4F76] rounded-full text-[#AB9F9D] font-bold w-10 h-10 flex items-center justify-center text-2xl">
+              <div className="bg-[#3C4F76] rounded-full text-[#EFF6EE] font-bold w-10 h-10 flex items-center justify-center text-2xl">
                 k
               </div>
               <div className=" border-[#3C4F76] border-2 rounded-full text-[#3C4F76] font-bold w-10 h-10 flex items-center justify-center text-2xl">
                 m
               </div>
-              <div className="bg-[#3C4F76] rounded-full text-[#AB9F9D] font-bold w-10 h-10 flex items-center justify-center text-2xl">
+              <div className="bg-[#3C4F76] rounded-full text-[#EFF6EE] font-bold w-10 h-10 flex items-center justify-center text-2xl">
                 k
               </div>
             </div>
-            <div className="rounded-full shadow-lg shadow-[#3C4F76] p-3 cursor-pointer">
+            <div
+              className="rounded-full shadow-lg shadow-[#3C4F76] p-3 cursor-pointer"
+              onClick={handleNav}
+            >
               <AiOutlineClose />
             </div>
           </div>
           <div className="border-b border-[#3c4f76]/50 mt-3">
-            <p>Enter programing saying here</p>
+            <p className="w-[85%] md:w-[90%] py-4">
+              Enter programing saying here
+            </p>
+          </div>
+          <div className="py-4 flex flex-col">
+            <ul>
+              <Link href="/">
+                <li className="py-4 text-sm">Home</li>
+              </Link>
+              <Link href="/">
+                <li className="py-4 text-sm">Skills</li>
+              </Link>
+              <Link href="/">
+                <li className="py-4 text-sm">Projects</li>
+              </Link>
+              <Link href="/">
+                <li className="py-4 text-sm">Skills</li>
+              </Link>
+              <Link href="/">
+                <li className="py-4 text-sm">Contact</li>
+              </Link>
+            </ul>
+            <div className="pt-20 sm:pt-40">
+              <p className="uppercase tracking-widest text-[#3c4f76]">
+                Let's connect
+              </p>
+              <div className="flex items-center justify-between py-4 w-full sm:w-[80%]">
+                <div className="rounded-full shadow-lg shadow-[#3c4f76] p-3 cursor-pointer hover:scale-105 ease-in duration-300">
+                  <AiOutlineLinkedin />
+                </div>
+                <div className="rounded-full shadow-lg shadow-[#3c4f76] p-3 cursor-pointer hover:scale-105 ease-in duration-300">
+                  <VscGithub />
+                </div>
+                <div className="rounded-full shadow-lg shadow-[#3c4f76] p-3 cursor-pointer hover:scale-105 ease-in duration-300">
+                  <FiMail />
+                </div>
+                <div className="rounded-full shadow-lg shadow-[#3c4f76] p-3 cursor-pointer hover:scale-105 ease-in duration-300">
+                  <BsPersonLinesFill />
+                </div>
+              </div>
+            </div>
           </div>
         </div>
       </div>
