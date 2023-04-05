5a56733944ee2a1c6ef68d15002cbf759adc962f
kyljmk <k.mansonkullin@googlemail.com>
2023-04-05 13:52:09 +0200

    Add Animations To Home Page

--- new-portfolio/src/components/Main.tsx
+++ new-portfolio/src/components/Main.tsx
@@ -3,11 +3,27 @@ import React from "react";
 function Main() {
   return (
     <div className="w-full h-screen text-center">
-      <div className="max-w-[1240px] w-full h-full mx-auto p-2 flex justify-center items-center">
-        <div>
-          <p className="uppercase text-sm tracking-widest text-[#383F51]">
-            Some programming saying
+      <div className="max-w-[1240px] w-full h-full mx-auto p-4 flex justify-center items-center">
+        <div className="text-[#383F51]">
+          <p className="text-sm tracking-widest py-2">
+            <span className="uppercase">
+              "Experience is the name everyone gives to their mistakes"
+            </span>
+            &nbsp;-&nbsp;Oscar&nbsp;Wilde
           </p>
+          <div className="flex flex-col items-center">
+            <div className="bg-[#3C4F76] rounded-full h-11 w-[370px] sm:h-14 absolute z-[-1] animate-slide-in-left"></div>
+            <div className="bg-[#3C4F76] rounded-full h-11 w-[340px] mt-12 sm:mt-16 sm:h-14 absolute z-[-1] animate-slide-in-right"></div>
+            <h1 className="tracking-widest py-1 text-[#A98743]">
+              My name is
+              <span className="text-[#EFF6EE]"> Kyle</span>
+            </h1>
+            <h1 className="text-[#EFF6EE] tracking-widest py-1">
+              Manson&#8209;Kullin
+            </h1>
+            <h1 className="tracking-widest py-1">I'm a full stack</h1>
+            <h1 className="tracking-widest py-1">developer.</h1>
+          </div>
         </div>
       </div>
     </div>
--- new-portfolio/tailwind.config.js
+++ new-portfolio/tailwind.config.js
@@ -2,7 +2,30 @@
 module.exports = {
   content: ["./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      keyframes: {
+        slideInLeft: {
+          "0%": {
+            transform: "translateX(-500px)",
+          },
+          "100%": {
+            transform: "translateX(0)",
+          },
+        },
+        slideInRight: {
+          "0%": {
+            transform: "translateX(500px)",
+          },
+          "100%": {
+            transform: "translateX(0)",
+          },
+        },
+      },
+      animation: {
+        "slide-in-left": "slideInLeft 1.5s linear",
+        "slide-in-right": "slideInRight 1.5s linear",
+      },
+    },
   },
   plugins: [],
 };
