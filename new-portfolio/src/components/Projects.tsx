c15d10748bbc73da9350a5aca7d2b40d6dd8bf93
kyljmk <k.mansonkullin@googlemail.com>
2023-04-08 23:02:56 +0200

    Add Basic Stylings To Projects Component

--- new-portfolio/src/components/Projects.tsx
+++ new-portfolio/src/components/Projects.tsx
@@ -7,8 +7,13 @@ function Projects() {
         <p className="text-xl tracking-widest uppercase text-[#3C4F76]">
           Projects
         </p>
-        <h2 className="py-4">What I've Built</h2>
-        <div className="grid md:grid-cols-2 gap-2"></div>
+        <div className="flex justify-center">
+          <div className="absolute mt-[10px] h-12 w-[290px] bg-[#3C4F76] rounded-full z-[-1] animate-slide-in-right"></div>
+          <h2 className="py-4 text-[#EFF6EE]">What I've Built</h2>
+        </div>
+        <div className="grid md:grid-cols-2 gap-8">
+          <div className="relative flex items-center justify-center h-auto w-full shadow-xl shadow-[#AB9F9D] rounded-xl p-4 group hover:bg-gradient-to-r from-[#3C4F76] to-[#383F51]"></div>
+        </div>
       </div>
     </div>
   );
